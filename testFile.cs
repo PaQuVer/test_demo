1bfda801deedbcb386967bd7800757888a6e1f21
PaQuVe <paquver@gmail.com>
2023-03-13 11:45:10 +0100

    Wrote base structure for c# program

--- testFile.cs
+++ testFile.cs
@@ -1,4 +1,9 @@
-namespace newNS
+namespace NewNS
 {
-    
+    class NS {
+        static void Main (string[] args)
+        {
+            System.Console.WriteLine("Hello world!");
+        }
+    }
 }
